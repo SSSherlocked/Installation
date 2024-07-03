254783245f9778749ce9de5d86964d296ddfa4d9
Xing-Zhao Jiang <35389477+SSSherlocked@users.noreply.github.com>
2024-07-04 00:17:45 +0800

    Update Install_CondaPackages.py

--- scripts/Install_CondaPackages.py
+++ scripts/Install_CondaPackages.py
@@ -4,7 +4,7 @@ import subprocess
 
 def install_package(package_name):
     try:
-        subprocess.check_output(["conda", "scripts", "-y", package_name])
+        subprocess.check_output(["conda", "install", "-y", package_name])
         print("Package installed successfully!")
     except subprocess.CalledProcessError:
         print("Package installation failed.")
