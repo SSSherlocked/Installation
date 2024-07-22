82875696be11c7b31057736161935b619167a174
Xing-Zhao Jiang <35389477+SSSherlocked@users.noreply.github.com>
2024-07-22 13:58:30 +0800

    Update Install_cmake.sh

--- scripts/Install_cmake.sh
+++ scripts/Install_cmake.sh
@@ -6,7 +6,7 @@ software_version="cmake-3.26.6"
 
 script_path="$(dirname "$(pwd)")/utils"
 
-source ${script_path}/setting.sh    "" ""
+source ${script_path}/setting.sh    "$1" "$2"
 source ${script_path}/download.sh   ${software_download_url}/${software_version} \
                                     ${package_dir}/${software_version} \
                                     ".tar.gz"
