c163bd0db6a05366963116ec3e56e23feba3ffbb
Spike <xzjiang@mail.dlut.edu.cn>
2024-09-05 15:04:22 +0800

    multi-thread make updated;

--- utils/install.sh
+++ utils/install.sh
@@ -10,10 +10,23 @@ function check() {
 # Use all CPU cores to compile
 function makeit() {
     local target=$1
+    local flag=1
+    if [ -z "${target}" ]; then
+        flag=0
+    fi
+
     if [ "$(uname)" == 'Darwin' ]; then
-        make -j $(sysctl -n hw.ncpu) "$target"
+        if [ $flag -eq 0 ]; then
+            make -j $(sysctl -n hw.ncpu)
+        else
+            make -j $(sysctl -n hw.ncpu) "$target"
+        fi
     else
-        make -j $(nproc) "$target"
+        if [ $flag -eq 0 ]; then
+            make -j $(nproc)
+        else
+            make -j $(nproc) "$target"
+        fi
     fi
 }
 
