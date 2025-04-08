c6184ddafc7bd4bf3086ec0f5ba860b24f5fd6d3
spike <xzjiang@mail.dlut.edu.cn>
2025-04-09 00:45:54 +0200

    Install_sundials.sh updated;

--- Install_sundials.sh
+++ Install_sundials.sh
@@ -7,10 +7,30 @@ software_download_url="https://github.com/LLNL/sundials/releases/download/v7.1.1
 software_version="sundials-7.1.1"
 
 
-install_flags="-D ENABLE_MPI=ON \
-               -D BUILD_FORTRAN_MODULE_INTERFACE=ON \
+install_flags="-D BUILD_FORTRAN_MODULE_INTERFACE=ON \
                -D EXAMPLES_ENABLE_C=ON \
-               -D EXAMPLES_ENABLE_CXX=ON"
+               -D EXAMPLES_ENABLE_CXX=ON \
+               -D ENABLE_CUDA=OFF \
+               -D ENABLE_GINKGO=OFF \
+               -D ENABLE_HIP=OFF \
+               -D ENABLE_HYPRE=OFF \
+               -D ENABLE_KLU=OFF \
+               -D ENABLE_KOKKOS=OFF \
+               -D ENABLE_KOKKOS_KERNELS=OFF \
+               -D ENABLE_LAPACK=OFF \
+               -D ENABLE_MAGMA=OFF \
+               -D ENABLE_MPI=ON \
+               -D ENABLE_ONEMKL=OFF \
+               -D ENABLE_OPENMP=OFF \
+               -D ENABLE_OPENMP_DEVICE=OFF \
+               -D ENABLE_PETSC=OFF \
+               -D ENABLE_PTHREAD=OFF \
+               -D ENABLE_RAJA=OFF \
+               -D ENABLE_SUPERLUDIST=OFF \
+               -D ENABLE_SUPERLUMT=OFF \
+               -D ENABLE_SYCL=OFF \
+               -D ENABLE_TRILINOS=OFF \
+               -D ENABLE_XBRAID=OFF"
 
 
 source "${script_path}/DirSetting.sh"       "${software}" \
